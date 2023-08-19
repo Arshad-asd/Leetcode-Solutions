0aa372e7f598849058fd3e07d0ab2b95548cdf7f
Arshad <arshaarshad21@mail.com>
2023-08-19 12:41:30 +0530

    QUESTION NO: 1. Two Sum

--- Python-Solutions/Leetcode_Array.py
+++ Python-Solutions/Leetcode_Array.py
@@ -1,4 +1,7 @@
                                             #LEETCODE ARAY QUESTIONS SOLVED
+
+'''--------------------------------------------------------------------------------------------'''
+
 # QUESTIOIN NO : 896. Monotonic Array
 
 class Solution:
@@ -12,5 +15,19 @@ class Solution:
                 decreasing = False
         
         return increasing or decreasing
+
+'''--------------------------------------------------------------------------------------------'''
+
+# QUESTION NO: 1. Two Sum
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return []
 
 '''--------------------------------------------------------------------------------------------'''
