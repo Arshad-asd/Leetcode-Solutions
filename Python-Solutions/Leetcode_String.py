59b83e2b4f9d987aa58af1c66f00c425f93419a1
Arshad A A <126140137+Arshad-asd@users.noreply.github.com>
2023-08-21 10:08:12 +0530

    Update Leetcode_String.py

--- Python-Solutions/Leetcode_String.py
+++ Python-Solutions/Leetcode_String.py
@@ -15,3 +15,18 @@ class Solution:
         
         return process_string(s) == process_string(t)
 '''--------------------------------------------------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER :: 917. Reverse Only Letters
+
+
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        alpha_chars = [ char for char in s if char.isalpha()]
+        reversed_chars = []
+        for char in s:
+            if char.isalpha():
+                reversed_chars.append(alpha_chars.pop())
+            else:
+                reversed_chars.append(char)
+        return ''.join(reversed_chars)
+
+'''--------------------------------------------------------------------------------------------------------------------------------------'''
