9c35e1acea51188ff882f7de6fba3a759feadead
Arshad <arshaarshad21@mail.com>
2023-08-23 13:05:38 +0530

    ispalindrome

---
+++ Python-Solutions/Leetcode_Numbers.py
@@ -0,0 +1,18 @@
+                                                    #LEETCODE ARAY QUESTIONS SOLVED
+
+'''--------------------------------------------------------------------------------------------'''
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        num, temp = 0, x
+        
+        while temp != 0:
+            num = num * 10 + temp % 10
+            temp //= 10
+        
+        return num == x
+        
+obj = Solution()
+print(obj.isPalindrome(121))
