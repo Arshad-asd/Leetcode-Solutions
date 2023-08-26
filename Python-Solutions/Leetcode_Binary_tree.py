21ac57b8253dc3a8e08b987913ace4efd7ed9e47
Arshad <arshaarshad21@gmail.com>
2023-08-26 11:20:20 +0530

    257. Binary Tree Paths

---
+++ Python-Solutions/Leetcode_Binary_tree.py
@@ -0,0 +1,25 @@
+                                                     # BINARY TREE LEETCODE SOLUTIONS
+'''--------------------------------------------------------------------------------------------'''
+
+# QUESTION NO: 257. Binary Tree Paths
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        result = []
+
+        def travers(root,path):
+            if not root:
+                return []
+            if not(root.left or root.right):
+                return result.append(path+str(root.val))
+            if root.left:
+                travers(root.left,path+str(root.val)+"->")
+            if root.right:
+                travers(root.right,path+str(root.val)+"->")
+        travers(root,"")
+        return result
