7258b5e09cc22c11add1b8f969484ef7acd50595
Arshad <arshaarshad21@gmail.com>
2023-09-06 10:31:02 +0530

    1103. Distrubute candies to people

--- Python-Solutions/Leetcode_Numbers.py
+++ Python-Solutions/Leetcode_Numbers.py
@@ -42,3 +42,24 @@ class Solution:
     
 '''--------------------------------------------------------------------------------------------'''
 
+# 1103. Distribute Candies to People
+
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        arr = [ 0 for i in range(num_people)]
+        i = 0
+        j = 1
+        while candies > 0:
+            if i == len(arr):
+                i = 0
+            if candies >= j:
+                arr[i] += j
+                candies -= j
+                j += 1
+            else:
+                arr[i] += candies
+                break
+            i += 1
+        return arr
+
+'''--------------------------------------------------------------------------------------------'''
