66047876ebe910b30b244f20c6833fc4b3fe90c1
Arshad <arshaarshad21@gmail.com>
2023-09-09 10:43:07 +0530

    2215. Find the Difference of Two Arrays

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -100,3 +100,22 @@ class Solution:
             return lists
 
 '''--------------------------------------------------------------------------------------------'''
+
+# QUESTION NO: 2215. Find the Difference of Two Arrays
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        nums1Set, nums2Set = set(nums1) , set(nums2)
+        res1, res2 = set(), set()
+
+        for n in nums1:
+            if n not in nums2Set:
+                res1.add(n)
+        
+        for n in nums2:
+            if n not in nums1Set:
+                res2.add(n)
+
+        return [list(res1), list(res2)]
+
+'''--------------------------------------------------------------------------------------------'''
