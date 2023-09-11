c73fba48e4058241380aae8bb610329e2703ef6b
Arshad <arshaarshad21@gmail.com>
2023-09-11 10:24:10 +0530

    2824. Count Pairs Whose Sum is Less than Target

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -119,3 +119,16 @@ class Solution:
         return [list(res1), list(res2)]
 
 '''--------------------------------------------------------------------------------------------'''
+
+# QUESTION NO : 2824. Count Pairs Whose Sum is Less than Target
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+               if nums[i] + nums[j] < target:
+                   count += 1
+        return count
+
+'''--------------------------------------------------------------------------------------------'''
