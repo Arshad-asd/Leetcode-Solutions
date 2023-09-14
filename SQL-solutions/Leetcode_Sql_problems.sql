ac2d155222c7429803003348b5f2c99cad457142
Arshad <arshaarshad21@gmail.com>
2023-09-14 10:53:43 +0530

    595. Big Countries

--- SQL-solutions/Leetcode_Sql_problems.sql
+++ SQL-solutions/Leetcode_Sql_problems.sql
@@ -9,3 +9,10 @@ SELECT product_id FROM Products WHERE low_fats = 'Y' and recyclable ="Y";
 
 SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS null;
 
+<-------------------------------------------------------------------------------------------------------------------------------->
+
+--Question NO : 595. Big Countries
+
+SELECT name,population,area FROM World WHERE area >= 3000000 OR population >= 25000000;
+
+<-------------------------------------------------------------------------------------------------------------------------------->
