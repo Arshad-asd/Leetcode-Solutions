6767d3262f6dffca9450afd2c97b61b6dfae207f
Arshad <arshaarshad21@gmail.com>
2023-09-14 10:42:17 +0530

    584. Find Customer Referee

--- SQL-solutions/Leetcode_Sql_problems.sql
+++ SQL-solutions/Leetcode_Sql_problems.sql
@@ -1,6 +1,11 @@
 <-------------------------------------------------------------------------------------------------------------------------------->
---1757. Recyclable and Low Fat Products
+--Question NO : 1757. Recyclable and Low Fat Products
 
 SELECT product_id FROM Products WHERE low_fats = 'Y' and recyclable ="Y";
 
 <-------------------------------------------------------------------------------------------------------------------------------->
+
+--Question NO : 584. Find Customer Referee
+
+SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS null;
+
