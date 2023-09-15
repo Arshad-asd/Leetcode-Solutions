1d8b9facbe1e64b1775fbb8e32808ed719cc2853
Arshad <arshaarshad21@gmail.com>
2023-09-15 11:15:28 +0530

    1581. Customer Who Visited but Did Not Make Any Transactions

--- SQL-solutions/Leetcode_Sql_problems.sql
+++ SQL-solutions/Leetcode_Sql_problems.sql
@@ -43,3 +43,14 @@ SELECT Product.product_name,Sales.year,Sales.price FROM Sales
 INNER JOIN Product ON Product.product_id = Sales.product_id;
 
 <-------------------------------------------------------------------------------------------------------------------------------->
+
+--Question NO : 1581. Customer Who Visited but Did Not Make Any Transactions
+
+SELECT v.customer_id, COUNT(v.visit_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
+
+<-------------------------------------------------------------------------------------------------------------------------------->
