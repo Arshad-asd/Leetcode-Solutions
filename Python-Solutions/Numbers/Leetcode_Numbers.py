a7350ee347096d78979b1fb04ed91cbd9ea2b279
Arshad <arshaarshad21@gmail.com>
2023-09-16 10:00:55 +0530

    2769. Find the Maximum Achievable Number

--- Python-Solutions/Numbers/Leetcode_Numbers.py
+++ Python-Solutions/Numbers/Leetcode_Numbers.py
@@ -80,3 +80,11 @@ class Solution:
         return result
        
 '''--------------------------------------------------------------------------------------------'''
+
+# Question No: 2769. Find the Maximum Achievable Number
+
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return  num + 2*t 
+
+'''--------------------------------------------------------------------------------------------'''
