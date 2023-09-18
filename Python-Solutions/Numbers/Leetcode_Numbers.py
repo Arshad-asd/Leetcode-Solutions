334106e640b48829ed17f0b5e9963264b72e68a8
Arshad <arshaarshad21@gmail.com>
2023-09-18 09:56:59 +0530

    2652. Sum Multiples

--- Python-Solutions/Numbers/Leetcode_Numbers.py
+++ Python-Solutions/Numbers/Leetcode_Numbers.py
@@ -88,3 +88,16 @@ class Solution:
         return  num + 2*t 
 
 '''--------------------------------------------------------------------------------------------'''
+
+# Question No: 2652. Sum Multiples
+
+
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        sums = 0
+        for i in range(1,n+1):
+           if i %3 ==0 or i % 5 == 0 or i % 7 == 0:
+                sums = sums + i
+        return sums
+
+'''--------------------------------------------------------------------------------------------'''
