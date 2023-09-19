a071c5ab27cdac31a53d1f31ea163b5a3df64e3d
Arshad <arshaarshad21@gmail.com>
2023-09-19 10:54:45 +0530

    204. Count Primes

--- Python-Solutions/Numbers/Leetcode_Numbers.py
+++ Python-Solutions/Numbers/Leetcode_Numbers.py
@@ -101,3 +101,43 @@ class Solution:
         return sums
 
 '''--------------------------------------------------------------------------------------------'''
+
+# Question No: 204. Count Primes
+ # setp 1
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+        primes = [1] * n
+        primes[0] = 0
+        primes[1] = 0
+        i = 2 
+        while i < n:
+            temp = i
+            if primes[i]:
+                temp += i
+                while temp < n:
+                    primes[temp] = 0
+                    temp += i
+            i = i + 1
+        return sum(primes)
+
+# step 2
+
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False
+
+        for i in range(2, int(n**0.5) + 1):
+            if is_prime[i]:
+                for j in range(i*i, n, i):
+                    is_prime[j] = False
+
+        return sum(is_prime)
+
+'''--------------------------------------------------------------------------------------------'''
