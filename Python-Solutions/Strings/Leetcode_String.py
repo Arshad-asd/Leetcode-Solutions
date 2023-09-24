157bd6c449dc6f4b687f06227b93f2037f024ccc
Arshad <arshaarshad21@gmail.com>
2023-09-24 12:38:49 +0530

    2788. Split Strings by Separator

--- Python-Solutions/Strings/Leetcode_String.py
+++ Python-Solutions/Strings/Leetcode_String.py
@@ -97,3 +97,17 @@ class Solution:
         return 1
 
 '''--------------------------------------------------------------------------------------------------------------------------------------'''
+
+#QUESTION NUMBER : 2788. Split Strings by Separator
+
+class Solution:
+    def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        ls = [] 
+        for data in words:
+            word = data.split(separator)
+            for data in word:
+                if(data != ""):
+                    ls.append(data)
+        return ls
+
+'''--------------------------------------------------------------------------------------------------------------------------------------'''
