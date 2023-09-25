5389198940ca2a27b7a6cbb63c8ad3aaa18fca4c
Arshad <arshaarshad21@gmail.com>
2023-09-25 13:05:32 +0530

    --Question NO : 1280. Students and Examinations

--- SQL-solutions/Leetcode_Sql_problems.sql
+++ SQL-solutions/Leetcode_Sql_problems.sql
@@ -54,3 +54,13 @@ WHERE t.transaction_id IS NULL
 GROUP BY v.customer_id;
 
 <-------------------------------------------------------------------------------------------------------------------------------->
+
+--Question NO : 1280. Students and Examinations
+
+
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
