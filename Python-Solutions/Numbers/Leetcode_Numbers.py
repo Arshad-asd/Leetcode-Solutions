5fef86e1ab2064d17af588260aaa4d225cd53f8f
Arshad <arshaarshad21@gmail.com>
2023-09-26 10:34:43 +0530

    2469. Convert the Temperature

--- Python-Solutions/Numbers/Leetcode_Numbers.py
+++ Python-Solutions/Numbers/Leetcode_Numbers.py
@@ -163,3 +163,11 @@ class Solution:
                     count += 1
 
         return count
+
+'''--------------------------------------------------------------------------------------------'''
+
+# Question No :2469. Convert the Temperature
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius + 273.15,celsius * 1.80 + 32.00]
