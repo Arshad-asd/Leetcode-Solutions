bc100c595b946aedcce5dd939b052302bd0327a2
Arshad <arshaarshad21@gmail.com>
2023-10-11 10:03:00 +0530

    1365. How Many Numbers Are Smaller Than the Current Number

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -236,3 +236,29 @@ class Solution:
        return list(names)
 
 '''--------------------------------------------------------------------------------------------'''
+
+# QUESTION NO : 1365. How Many Numbers Are Smaller Than the Current Number
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        result = []
+        for i in range(len(nums)):
+            count = 0
+            for j in range(len(nums)):
+                if nums[j] < nums[i] and  j != i:
+                    count += 1
+            result.append(count)
+        return result
+
+'''--------------------------------------------------------------------------------------------'''
+
+# QUESTION NO : 1389. Create Target Array in the Given Order
+
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+       target = []
+       for n,i in zip(nums,index):
+           target.insert(i,n)
+       return target
+
+'''--------------------------------------------------------------------------------------------'''
