e978aa0c6cd342e3f1c913cab6859de902ad857a
Arshad <arshaarshad21@gmail.com>
2023-10-18 09:55:37 +0530

    2894. Divisible and Non-divisible Sums Difference

--- Python-Solutions/Numbers/Leetcode_Numbers.py
+++ Python-Solutions/Numbers/Leetcode_Numbers.py
@@ -181,3 +181,17 @@ class Solution:
         return n if n % 2 == 0 else n * 2
 
 '''--------------------------------------------------------------------------------------------'''
+# Question No : 2894. Divisible and Non-divisible Sums Difference
+
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1 = 0
+        num2 = 0
+        for i in range(1,n+1):
+            if i % m == 0:
+                num2 += i
+            else:
+                num1 += i
+        return num1 - num2 
+
+'''--------------------------------------------------------------------------------------------'''
