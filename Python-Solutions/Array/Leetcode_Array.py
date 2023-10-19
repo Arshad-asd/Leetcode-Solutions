47a1bc56a410e9bdee9562f17a85afd2be910129
Arshad <arshaarshad21@gmail.com>
2023-10-19 10:07:44 +0530

    1920. Build Array from Permutation

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -288,3 +288,12 @@ class Solution:
         return res
 
 '''--------------------------------------------------------------------------------------------'''
+# QUESTION NO : 1920. Build Array from Permutation
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+       ans = []
+       for i in range(len(nums)):
+           ans.append(nums[nums[i]])
+       return ans
+
+'''--------------------------------------------------------------------------------------------'''
