dc36b74a87ba6fa8e3fb40d735c158bf57b4bb09
Arshad <arshaarshad21@gmail.com>
2023-10-23 09:35:27 +0530

    1313. Decompress Run-Length Encoded List

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -322,3 +322,16 @@ class Solution:
         return count 
 
 '''--------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER : 1313. Decompress Run-Length Encoded List
+
+
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        generated = []
+        for i in range(0,len(nums),2):
+            freq = nums[i]
+            val = nums[i+1]
+            generated += [val] * freq
+        return generated
+
+'''--------------------------------------------------------------------------------------------'''
