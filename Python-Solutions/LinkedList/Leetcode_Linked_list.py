2a5ac988057ef1b7543763e223dcce74e8d2e023
Arshad <arshaarshad21@gmail.com>
2023-10-25 10:39:13 +0530

    2236. Root Equals Sum of Children

--- Python-Solutions/LinkedList/Leetcode_Linked_list.py
+++ Python-Solutions/LinkedList/Leetcode_Linked_list.py
@@ -126,3 +126,16 @@ class Solution:
         return dummy.next
 
 '''--------------------------------------------------------------------------------------------'''
+# QUESTION NO: 2236. Root Equals Sum of Children
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        if root.val == root.left.val + root.right.val:
+            return True
+        else:
+            return False
+        
