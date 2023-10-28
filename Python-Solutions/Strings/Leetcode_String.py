b5da2528ad0bdacba20b8d82426c34582065907c
Arshad <arshaarshad21@gmail.com>
2023-10-28 10:49:25 +0530

    1844. Replace All Digits with Characters

--- Python-Solutions/Strings/Leetcode_String.py
+++ Python-Solutions/Strings/Leetcode_String.py
@@ -223,3 +223,15 @@ class Solution:
 
         return result
 '''--------------------------------------------------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER : 1844. Replace All Digits with Characters
+class Solution:
+    def replaceDigits(self, s: str) -> str:
+        result =''
+        for i in range(len(s)):
+            if s[i].isnumeric() == True:
+                result += chr(ord(s[i-1])+int(s[i]))
+            else:
+                result +=s[i]
+        return result
+
+'''--------------------------------------------------------------------------------------------------------------------------------------'''
