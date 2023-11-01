795acac37244a0c8cf5c916bdc1e3545d60d1cb5
Arshad <arshaarshad21@gmail.com>
2023-11-01 09:44:34 +0530

    Final Value of Variable After Performing Operations

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -382,3 +382,17 @@ class Solution:
 
 
 '''--------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER  : 2011. Final Value of Variable After Performing Operations
+
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for i in operations:
+            if i == '--X' or i == 'X--':
+                x -= 1
+            if i == '++X' or i == 'X++':
+                x += 1
+        return x
+
+'''--------------------------------------------------------------------------------------------'''
