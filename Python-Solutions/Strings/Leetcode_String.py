84829b8f7067c9d2b7db05827638f44fbdde8f92
Arshad <arshaarshad21@gmail.com>
2023-11-05 20:37:37 +0530

    1812. Determine Color of a Chessboard Square

--- Python-Solutions/Strings/Leetcode_String.py
+++ Python-Solutions/Strings/Leetcode_String.py
@@ -235,3 +235,17 @@ class Solution:
         return result
 
 '''--------------------------------------------------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER  : 1812. Determine Color of a Chessboard Square
+
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        alpha = ['a','b','c','d','e','f','g','h']
+        for i in range(len(alpha)):
+            for j in range(1,9):
+                if alpha[i] + str(j) == coordinates:
+                    if (i+j) %  2 == 0:
+                        return True
+        else:
+            return False
+
+'''--------------------------------------------------------------------------------------------------------------------------------------'''
