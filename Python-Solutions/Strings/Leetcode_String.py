3f3cbf39058c71a98bda6cf82a18ef334bc65588
Arshad <arshaarshad21@gmail.com>
2023-11-06 09:07:18 +0530

    1374. Generate a String With Characters That Have Odd Counts

--- Python-Solutions/Strings/Leetcode_String.py
+++ Python-Solutions/Strings/Leetcode_String.py
@@ -249,3 +249,11 @@ class Solution:
             return False
 
 '''--------------------------------------------------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER  : 1374. Generate a String With Characters That Have Odd Counts
+
+class Solution:
+    def generateTheString(self, n: int) -> str:
+        return "a" * n if  n % 2 else "a" * (n-1) +"b"
+
+
+'''--------------------------------------------------------------------------------------------------------------------------------------'''
