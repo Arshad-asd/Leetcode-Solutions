28bbd2a1b790d8b3acecd06f380a9e12a51b204e
Arshad <arshaarshad21@gmail.com>
2023-11-07 10:14:25 +0530

    1051. Height Checker

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -419,6 +419,15 @@ class Solution:
 
 '''--------------------------------------------------------------------------------------------'''
 
+#QUESTION NUMBER  : 1051. Height Checker
 
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+        count = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                count += 1
+        return count
 
 '''--------------------------------------------------------------------------------------------'''
