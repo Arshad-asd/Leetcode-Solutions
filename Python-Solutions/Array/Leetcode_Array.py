978d101aeee7a12065b1041c239d0e614314494a
Arshad <arshaarshad21@gmail.com>
2023-11-13 09:42:46 +0530

    1450. Number of Students Doing Homework at a Given Time

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -442,3 +442,11 @@ class Solution:
          return res
 
 '''--------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER  : 1450. Number of Students Doing Homework at a Given Time
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+          count = 0
+          for i in range(len(startTime)):
+              if endTime[i] >= queryTime and startTime[i] <= queryTime:
+                  count += 1
+          return count
