0d430daa36760ba19397020f728c34a1c2b402b2
Arshad <arshaarshad21@gmail.com>
2023-11-16 10:11:59 +0530

    2357. Make Array Zero by Subtracting Equal Amounts

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -451,6 +451,7 @@ class Solution:
                   count += 1
           return count
 
+'''--------------------------------------------------------------------------------------------'''
 #QUESTION NUMBER  : 2357. Make Array Zero by Subtracting Equal Amounts
 
 class Solution:
@@ -483,3 +484,12 @@ class Solution:
             count_operations += 1
 
         return count_operations
+
+'''--------------------------------------------------------------------------------------------'''
+
+#QUESTION NUMBER  : 2154. Keep Multiplying Found Values by Two
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        while original in nums:
+            original *= 2
+        return original
