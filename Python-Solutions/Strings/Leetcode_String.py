94e6fb56dbb247f57213e905b2ba65a2513f495d
Arshad <arshaarshad21@gmail.com>
2023-12-24 14:33:56 +0530

    771. Jewels and Stones

--- Python-Solutions/Strings/Leetcode_String.py
+++ Python-Solutions/Strings/Leetcode_String.py
@@ -403,3 +403,15 @@ class Solution:
             if current_word[:len(pref)] == pref:
                 count += 1
         return count
+
+'''--------------------------------------------------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER :771. Jewels and Stones
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for i in stones:
+            if i in jewels:
+                count += 1
+        return count
+'''--------------------------------------------------------------------------------------------------------------------------------------'''
