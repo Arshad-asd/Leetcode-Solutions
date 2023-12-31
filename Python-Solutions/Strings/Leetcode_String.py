ba3616e7e97bf11e4f5654e5f68f3c5086c52b7a
Arshad <arshaarshad21@gmail.com>
2023-12-31 16:40:48 +0530

    1768. Merge Strings Alternately

--- Python-Solutions/Strings/Leetcode_String.py
+++ Python-Solutions/Strings/Leetcode_String.py
@@ -415,3 +415,13 @@ class Solution:
                 count += 1
         return count
 '''--------------------------------------------------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER : 1768. Merge Strings Alternately
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = ''
+        n = min(len(word1), len(word2))
+        for i in range(n):
+                res += word1[i] + word2[i]
+        res += word1[n:] + word2[n:]
+
+        return res
