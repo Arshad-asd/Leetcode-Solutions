ee93043269818ffd9f87358327868bc461061ed2
Arshad <arshaarshad21@gmail.com>
2024-01-12 11:41:38 +0530

    update solutions

--- Python-Solutions/Strings/Leetcode_String.py
+++ Python-Solutions/Strings/Leetcode_String.py
@@ -473,3 +473,18 @@ class Solution:
             if len(arr)-1 != i:
                result += " "
         return result
+
+'''--------------------------------------------------------------------------------------------------------------------------------------'''
+
+#QUESTION NUMBER : 2399. Check Distances Between Same Letters
+
+class Solution:
+    def checkDistances(self, s: str, distance: List[int]) -> bool:
+            unique = set(s)
+            for letter in unique:
+                index = s.index(letter) # get the first index of the letter
+                dis = distance[ord(letter)-97]
+                if index+dis+1 > len(s)-1 or s[index+dis+1] != letter:
+                    return False
+            return True
+'''--------------------------------------------------------------------------------------------------------------------------------------'''
