b348e1abcd8da7206e76996d83b1e926286eb6ca
Arshad <arshaarshad21@gmail.com>
2024-01-26 09:44:58 +0530

    1370. Increasing Decreasing String

--- Python-Solutions/Strings/Leetcode_String.py
+++ Python-Solutions/Strings/Leetcode_String.py
@@ -488,3 +488,17 @@ class Solution:
                     return False
             return True
 '''--------------------------------------------------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER : 1370. Increasing Decreasing String
+
+class Solution:
+    def sortString(self, s: str) -> str:
+        s = list(s)
+        result = ''
+        while s:
+            for letter in sorted(set(s)):
+                s.remove(letter)
+                result += letter
+            for letter in sorted(set(s),reverse=True):
+                s.remove(letter)
+                result += letter
+        return result
