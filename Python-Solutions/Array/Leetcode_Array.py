782f3fe51d85070fe5fd50fd860840af77f551e9
Arshad <arshaarshad21@gmail.com>
2024-02-08 09:38:45 +0530

    268. Missing Number

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -648,3 +648,16 @@ class Solution:
         return peak
             
 '''--------------------------------------------------------------------------------------------'''
+#QUESTION NUMBER  : 268. Missing Number
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        left,right = 0,len(nums)
+        while left < right:
+            mid = (left+right)//2
+            if nums[mid] > mid:
+                right = mid
+            else:
+                left = mid+1
+        return left
+                
