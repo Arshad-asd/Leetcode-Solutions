9102359df04bdd64034e1a5d2c352c233dc78df7
Arshad <arshaarshad21@gmail.com>
2024-03-04 10:30:06 +0530

    49. Group Anagrams

--- Python-Solutions/Array/Leetcode_Array.py
+++ Python-Solutions/Array/Leetcode_Array.py
@@ -777,3 +777,15 @@ class Solution:
             for j in range(len(nums)-i-1):
                 if nums[j] > nums[j+1]:
                     nums[j],nums[j+1] = nums[j+1],nums[j]
+
+#QUESTION NUMBER  :  49. Group Anagrams
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in anagrams:
+                anagrams[sorted_word].append(word)
+            else:
+                anagrams[sorted_word] = [word]
+        return list(anagrams.values())
