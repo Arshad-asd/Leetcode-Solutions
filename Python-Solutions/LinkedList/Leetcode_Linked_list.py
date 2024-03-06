a761942f70ce841c3b2b3294f52f4ad8c341a46e
Arshad <arshaarshad21@gmail.com>
2024-03-06 17:57:47 +0530

    141. Linked List Cycle

--- Python-Solutions/LinkedList/Leetcode_Linked_list.py
+++ Python-Solutions/LinkedList/Leetcode_Linked_list.py
@@ -285,3 +285,17 @@ class Solution:
                 last_sorted = last_sorted.next
         
         return dummy.next
+
+# QUESTION NO 141. Linked List Cycle
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            if fast == slow:
+                return True
+            slow = slow.next
+            fast = fast.next.next
+        return False
