2764866f9b53ea85d7670759436630dda234ce36
Arshad <arshaarshad21@gmail.com>
2024-03-07 09:16:25 +0530

    876. Middle of the Linked List

--- Python-Solutions/LinkedList/Leetcode_Linked_list.py
+++ Python-Solutions/LinkedList/Leetcode_Linked_list.py
@@ -299,3 +299,13 @@ class Solution:
             slow = slow.next
             fast = fast.next.next
         return False
+
+# QUESTION NO 141. 876. Middle of the Linked List
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
