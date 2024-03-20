16baaa974c25b0de4b4b82fb2328ddf23bf01c38
Arshad <arshaarshad21@gmail.com>
2024-03-20 09:42:16 +0530

    38. Count and Say

--- Python-Solutions/Strings/Leetcode_String.py
+++ Python-Solutions/Strings/Leetcode_String.py
@@ -615,3 +615,18 @@ class Solution:
                 if not stack or stack.pop() != bracket_pairs[char]:
                     return False
         return not stack
+#QUESTION NUMBER  : 38. Count and Say
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n==1:
+            return '1'
+        prev = self.countAndSay(n-1)
+        result = ''
+        count = 1
+        for i in range(len(prev)):
+            if i < len(prev)-1 and prev[i] == prev[i+1]:
+                count += 1
+            else:
+                result += str(count)+prev[i]
+                count = 1
+        return result
